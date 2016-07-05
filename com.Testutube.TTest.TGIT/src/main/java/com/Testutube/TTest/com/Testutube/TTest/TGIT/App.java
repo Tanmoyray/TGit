f83da31eb60d5ac600e1c0dfb777ff020d127137
Tanmoyray <ray.tanmoy@senrysa.com>
2016-07-05 18:59:05 +0530

    add app.java and apptest.java class file

---
+++ com.Testutube.TTest.TGIT/src/main/java/com/Testutube/TTest/com/Testutube/TTest/TGIT/App.java
@@ -0,0 +1,16 @@
+package com.Testutube.TTest.com.Testutube.TTest.TGIT;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Test
+	public void TestApp( )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
